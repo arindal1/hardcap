31e7818de463ff52fbc36177ccd004382257f631
arindal1 <arindalchar17@gmail.com>
2026-09-01 22:09:26 +0530

    set `trustHost` to true

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,6 +16,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   pages: { signIn: "/login" },
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
